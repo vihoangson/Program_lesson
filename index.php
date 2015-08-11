061aeafbf5feef175441190d3d47f700e46305c7
CBA_Santo <santo@cybridge.jp>
2015-08-11 17:11:31 +0900

    commit

--- index.php
+++ index.php
@@ -57,22 +57,22 @@
 						</div>
 						<div class="col-md-3">
 							<ul class="nav nav-pills nav-stacked">
-								<li><a href="nangluctieman.com" target="_blank">Nangluctieman</a></li>
-								<li><a href="banhsusu.com" target="_blank">Nanhsusu</a></li>
-								<li><a href="dacsandaklak.vus.vn" target="_blank">Dacsandaklak</a></li>
+								<li><a href="http://nangluctieman.com" target="_blank">Nangluctieman</a></li>
+								<li><a href="http://banhsusu.com" target="_blank">Nanhsusu</a></li>
+								<li><a href="http://dacsandaklak.vus.vn" target="_blank">Dacsandaklak</a></li>
 							</ul>
 						</div>
 						<div class="col-md-3">
 							<ul class="nav nav-pills nav-stacked">
-								<li><a href="hocboi.vus.vn" target="_blank">Hocboi</a></li>
-								<li><a href="phapthi.vus.vn" target="_blank">Phapthi</a></li>
-								<li><a href="santo.vus.vn" target="_blank">Santo</a></li>
+								<li><a href="http://hocboi.vus.vn" target="_blank">Hocboi</a></li>
+								<li><a href="http://phapthi.vus.vn" target="_blank">Phapthi</a></li>
+								<li><a href="http://santo.vus.vn" target="_blank">Santo</a></li>
 							</ul>
 						</div>
 						<div class="col-md-3">
 							<ul class="nav nav-pills nav-stacked">
-								<li><a href="vihoangson.vus.vn" target="_blank">Vihoangson</a></li>
-								<li><a href="tainangtieman.com" target="_blank">Tainangtieman</a></li>
+								<li><a href="http://vihoangson.vus.vn" target="_blank">Vihoangson</a></li>
+								<li><a href="http://tainangtieman.com" target="_blank">Tainangtieman</a></li>
 							</ul>
 						</div>
 					</div>
