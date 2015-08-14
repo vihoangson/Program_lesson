1e2f2e6bbc6854ee1df0b15c0268efdacba39233
CBA_Santo <santo@cybridge.jp>
2015-08-14 10:23:08 +0900

    commit

--- Lesson_1/show_img.php
+++ Lesson_1/show_img.php
@@ -1,6 +1,42 @@
 <?php
-	$im = new imagick( "store/".$_GET["file"] );
-	$im->cropThumbnailImage( 80, 80 );
-	header("Content-Type: image/jpg");
-	echo $im->getImageBlob();
+$type_image	= "common"; //(common || imagemagick)
+$url		= "store/".$_GET["file"];
+$width		= 80;
+$height		= 80;
+
+switch($type_image){
+	case "imagemagick":
+		$im = new imagick($url);
+		$im->cropThumbnailImage( 80, 80 );
+		header("Content-Type: image/jpg");
+		echo $im->getImageBlob();
+	break;
+	case "common":
+		// Le fichier
+		$filename	= $url;
+		// Définition de la largeur et de la hauteur maximale
+		$width		= 80;
+		$height		= 80;
+		// Content type
+		header('Content-Type: image/jpeg');
+		// Cacul des nouvelles dimensions
+		list($width_orig, $height_orig) = getimagesize($filename);
+		$ratio_orig = $width_orig/$height_orig;
+
+		if ($width/$height > $ratio_orig) {
+			$width = $height*$ratio_orig;
+		} else {
+			$height = $width/$ratio_orig;
+		}
+		// Redimensionnement
+		$image_p = imagecreatetruecolor($width, $height);
+		$image = imagecreatefromjpeg($filename);
+		imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+
+		// Affichage
+		imagejpeg($image_p, null, 100);
+	break;
+}
+
+
 ?>
