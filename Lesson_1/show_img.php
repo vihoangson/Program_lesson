fdbfb98a4b717054abda76b7c1e87f9a3042ff8a
CBA_Santo <santo@cybridge.jp>
2015-08-14 10:57:10 +0900

    commit

--- Lesson_1/show_img.php
+++ Lesson_1/show_img.php
@@ -1,42 +1,40 @@
 <?php
 $type_image	= "common"; //(common || imagemagick)
-$url		= "store/".$_GET["file"];
+$filename		= "store/".$_GET["file"];
 $width		= 80;
 $height		= 80;
 
 switch($type_image){
 	case "imagemagick":
-		$im = new imagick($url);
-		$im->cropThumbnailImage( 80, 80 );
+		$im = new imagick($filename);
+		$im->cropThumbnailImage( $width, $height );
 		header("Content-Type: image/jpg");
 		echo $im->getImageBlob();
 	break;
 	case "common":
-		// Le fichier
-		$filename	= $url;
-		// Définition de la largeur et de la hauteur maximale
-		$width		= 80;
-		$height		= 80;
-		// Content type
-		header('Content-Type: image/jpeg');
-		// Cacul des nouvelles dimensions
-		list($width_orig, $height_orig) = getimagesize($filename);
-		$ratio_orig = $width_orig/$height_orig;
-
-		if ($width/$height > $ratio_orig) {
-			$width = $height*$ratio_orig;
-		} else {
-			$height = $width/$ratio_orig;
-		}
-		// Redimensionnement
-		$image_p = imagecreatetruecolor($width, $height);
-		$image = imagecreatefromjpeg($filename);
-		imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
-
-		// Affichage
-		imagejpeg($image_p, null, 100);
+		cropImage_common($filename,$width,$height);
 	break;
 }
 
+function cropImage_common($filename,$width,$height){
+	// Content type
+	header('Content-Type: image/jpeg');
+	// Cacul des nouvelles dimensions
+	list($width_orig, $height_orig) = getimagesize($filename);
+	$ratio_orig = $width_orig/$height_orig;
+
+	if ($width/$height > $ratio_orig) {
+		$width = $height*$ratio_orig;
+	} else {
+		$height = $width/$ratio_orig;
+	}
+	// Redimensionnement
+	$image_p = imagecreatetruecolor($width, $height);
+	$image = imagecreatefromjpeg($filename);
+	imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+
+	// Affichage
+	imagejpeg($image_p, null, 100);
+}
 
 ?>
--- Lesson_4/show_img.php
+++ Lesson_4/show_img.php
@@ -1,27 +1,53 @@
 <?php 
 	$case				= "show_img";//show_img, save_img
 	$cropThumbnailImage	= true;
+	$filename			= "img/".$_GET["file"] ;
 
-	$im = new imagick( "img/".$_GET["file"] );
+	if(class_exists("imagick")){
+		$im = new imagick($filename);
 
-	/* create the thumbnail */
-	if($cropThumbnailImage){
-		$im->cropThumbnailImage( 80, 80 );
+		/* create the thumbnail */
+		if($cropThumbnailImage){
+			$im->cropThumbnailImage( 80, 80 );
+		}
+
+		/* Write to a file */
+		if($resizeImage){
+			$im->resizeImage(900,80,1,0.5);
+		}
+
+		switch($case){
+			case "write_img":
+				$im->writeImage( "img/".$_GET["file"] );
+			break;
+			case "show_img":
+				header("Content-Type: image/jpg");
+				echo $im->getImageBlob();
+			break;
+		}
+	}else{
+		cropImage_common($filename,80,80);
 	}
 
-	/* Write to a file */
-	if($resizeImage){
-		$im->resizeImage(900,80,1,0.5);
-	}
+	function cropImage_common($filename,$width,$height){
+		// Content type
+		header('Content-Type: image/jpeg');
+		// Cacul des nouvelles dimensions
+		list($width_orig, $height_orig) = getimagesize($filename);
+		$ratio_orig = $width_orig/$height_orig;
 
-	switch($case){
-		case "write_img":
-			$im->writeImage( "img/".$_GET["file"] );
-		break;
-		case "show_img":
-			header("Content-Type: image/jpg");
-			echo $im->getImageBlob();
-		break;
+		if ($width/$height > $ratio_orig) {
+			$width = $height*$ratio_orig;
+		} else {
+			$height = $width/$ratio_orig;
+		}
+		// Redimensionnement
+		$image_p = imagecreatetruecolor($width, $height);
+		$image = imagecreatefromjpeg($filename);
+		imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+
+		// Affichage
+		imagejpeg($image_p, null, 100);
 	}
  ?>
  
