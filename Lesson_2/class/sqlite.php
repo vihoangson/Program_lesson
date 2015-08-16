b82fcd55857c66ff8608b914f527ae7185f97266
vihoangson <vihoangson@gmail.com>
2015-08-16 13:05:58 +0700

    commit

--- Lesson_2/class/sqlite.php
+++ Lesson_2/class/sqlite.php
@@ -3,7 +3,7 @@
 		public function __construct()
 		{
 			define("PATH_UPLOAD", $_SERVER["DOCUMENT_ROOT"]."/Lesson_2/db/");
-			$this->open(PATH_UPLOAD.'lesson_2.db.sqlite');
+			$this->open(PATH_UPLOAD.'lesson_2.db.sqlites');
 		}
 
 		public function fetchAll($result){
