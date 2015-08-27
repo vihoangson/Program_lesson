7493ddadf89346a95d14ca148fc8d856b4188c53
CBA_Santo <santo@cybridge.jp>
2015-08-27 10:06:10 +0900

    commit

--- Test_case_1/class/db.php
+++ Test_case_1/class/db.php
@@ -34,35 +34,15 @@ class Db_csv{
 	}
 
 	function getAllDataPager($per_page=10,$page=0){
-		// $offset		= $page*$per_page;
-		// $value		= array_values($this->data);
-		// $all_row	= count($value);
-		// $all_page	= round($all_row/$per_page);//allpage
-		// $distance	=3;
-		// $page_li[]=1;
-		// if($page>$distance + 1){
-		// 	$page_li[]="...";
-		// }
-		// for($i=2;$i<$all_page;$i++){
-		// 	if($i > $page-$distance  && $i < $page+$distance ){
-		// 		$page_li[]=$i;
-		// 	}
-		// }
-		// if($page<$all_page-$distance ){
-		// 	$page_li[]="...";
-		// }
-		// $page_li[] = $all_page;
-		// foreach ($page_li as $key => $value) {
-		// 	$html .= "<li><a ".( (is_numeric($value) && $page != $value) ? "href='?page=".$value :"." )." '>".$value."</a></li>";
-		// }
-		
+		$value		= array_values($this->data);
+		$offset		= $page*$per_page;
+
 		$params["page"]			= $page;
 		$params["per_page"]		= $per_page;
-		$value		= array_values($this->data);
 		$params["all_row"]		= count($value);
 		$params["distance"]		= 5;
 		$html = $this->paranation($params);
-		for($i=0;$i<=$per_page;$i++){
+		for($i=0;$i<$per_page;$i++){
 			$return["data"][$value[$i+$offset]["id"]] = $value[$i+$offset];
 		}
 
