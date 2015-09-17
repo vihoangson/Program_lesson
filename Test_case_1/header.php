a74212304220449e8e21e623735c4b201c2376ac
CBA_Santo <santo@cybridge.jp>
2015-09-17 10:54:42 +0900

    firewall script

--- Test_case_1/header.php
+++ Test_case_1/header.php
@@ -1,4 +1,19 @@
 <?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$maxRequestsAllowed = 2000;
+$ips = @unserialize(file_get_contents("/var/www/vhosts/santo/Program_lesson/Test_case_1/ipban.data"));
+if (!is_array($ips)) {
+  $ips = array();
+}
+if (!isset($ips[$ip])) {
+  $ips[$ip] = 0;
+}
+$ips[$ip] += 1;
+file_put_contents('/var/www/vhosts/santo/Program_lesson/Test_case_1/ipban.data', serialize($ips));
+if ($ips[$ip] > $maxRequestsAllowed) {
+  die;
+}
+//==========================
 session_start();
 //Testmod to login
 $security_mod = false;
