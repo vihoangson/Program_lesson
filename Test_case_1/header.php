b5afa2f3361a568fc016e0c9983b5f3461c75555
CBA_Santo <santo@cybridge.jp>
2015-09-17 19:50:18 +0900

    commit

--- Test_case_1/header.php
+++ Test_case_1/header.php
@@ -9,14 +9,12 @@ if (!is_array($ips)) {
 if (!isset($ips[$ip])) {
 	$ips[$ip] = 0;
 }
-if($_GET["test_mode"]==1){
-	print_r($ips);
-}
 $ips[$ip] += 1;
-file_put_contents(__DIR__.'/ipban.data', serialize($ips));
 if ($ips[$ip] > $maxRequestsAllowed) {
 	echo "Waiting";
+	die;
 }
+file_put_contents(__DIR__.'/ipban.data', serialize($ips));
 //==============================================================================
 session_start();
 //Testmod to login
