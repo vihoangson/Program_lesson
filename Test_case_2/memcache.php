b24a2da94864aeac6ac29cbbabefb8c54d50ebc5
CBA_Santo <santo@cybridge.jp>
2015-10-08 16:30:21 +0900

    commit

--- Test_case_2/memcache.php
+++ Test_case_2/memcache.php
@@ -2,34 +2,8 @@
 $servers = array(
     array('localhost', 11211, 33),
 );
-$memcacheD = new Memcached;
-$memcacheD->addServers($servers);
+$memcached = new Memcached;
+$memcached->addServers($servers);
+//$memcached->set("son","deptrai");
+echo $memcached->get("son");
 
-$checks = array(
-    123,
-    4542.32,
-    'a string',
-    true,
-    array(123, 'string'),
-    (object)array('key1' => 'value1'),
-);
-foreach ($checks as $i => $value) {
-    print "Checking WRITE with Memcache\n";
-    $key = 'cachetest' . $i;
-    usleep(100);
-    $valD = $memcacheD->get($key);
-    if ($val !== $valD) {
-        print "Not compatible!";
-        var_dump(compact('val', 'valD'));
-    }
-
-    print "Checking WRITE with MemcacheD\n";
-    $key = 'cachetest' . $i;
-    //$memcacheD->set($key, $value);
-    usleep(100);
-    $valD = $memcacheD->get($key);
-    if ($val !== $valD) {
-        print "Not compatible!";
-        var_dump(compact('val', 'valD'));
-    }
-}
