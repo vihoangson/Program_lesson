103368feab84c0a25496e783cd23caba9ed55118
CBA_Santo <santo@cybridge.jp>
2015-10-08 10:13:33 +0900

    Lesson 8

--- Lesson_6/index.php
+++ Lesson_6/index.php
@@ -1,4 +1,14 @@
+<?php # Collect a specific users GEOIP info
+$info = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
+print_r ($info);
 
+# To get the info from one specific field
+$country = $info['country_name'];
+echo $country;
+
+# To combine information from the array into a string
+$info = implode("/", $info);
+echo $info; ?>
 <!DOCTYPE html>
 <html lang="vi">
 	<head>
--- Lesson_8/index.php
+++ Lesson_8/index.php
@@ -1,30 +1,10 @@
-<?php
-namespace Santo123;
-	require("lib/lib.php");
-	require("lib/lib2.php");
-	use My\Db\Santo ;
-	use My\Db\Santo2 ;
+<?php 
+require "vendor/autoload.php";
+use \Michelf\MarkdownExtra;
 
-	$a = new demo;
-	$b = new My\Db\Santo\demo;
-	$c = new My\Db\Santo2\demo;
+$my_text="
+asdfsdf
+==========
+";
 
-	echo "<hr>";
-	$b->demo();
-	echo "<br>";
-	$c->demo();
-	echo "<hr>";
-
-	echo My\Db\Santo\HELLO."<br>";
-	echo My\Db\Santo2\HELLO."<br>";
-
-/**
-*
-*/
-class demo
-{
-	function __construct()
-	{
-		echo "first_echo<br>";
-	}
-}
+echo MarkdownExtra::defaultTransform($my_text);
